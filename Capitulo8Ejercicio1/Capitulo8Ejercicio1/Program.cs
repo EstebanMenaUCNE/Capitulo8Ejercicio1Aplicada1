37239cc12036455a09b2381c47081e623d639411
DESKTOP-4V48SB7\Equipo <esteban.smb.3008@gmail.com>
2017-01-21 15:46:55 -0400

    Listo

---
+++ Capitulo8Ejercicio1/Capitulo8Ejercicio1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Capitulo8Ejercicio1
+{
+    class Program
+    {
+        //Hacer un programa que le pida al usuario una cadena y muestre cada una de las palabras de la cadena en una línea.
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite la cadena: ");
+            string cad = Console.ReadLine();
+            Console.WriteLine("\nCadena por palabras: \n");
+            foreach (char c in cad)
+            {
+                if (c.Equals(' '))
+                    Console.WriteLine();
+                else
+                    Console.Write(c);
+            }
+            Console.ReadKey();
+        }
+    }
+}
